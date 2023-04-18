148f5f9b4ae85ba144043d108b5a95782480ce0d
Ivan Murenko <ivan.murenko@sonarsource.com>
2023-04-18 16:58:42 +0200

    Red qg

--- MainTest.java
+++ MainTest.java
@@ -1,8 +1,9 @@
 public class MainTest {
 
   public static void main(String[] args) {
-    int z = 0;
-    System.out.println(1/z);
+    while (true) {
+      System.out.println("1");
+    }
   }
 
 }
